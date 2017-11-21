680ea6b721eb193e94fc298145d1e3eaf759b98b
Anton Tregubov <tregubov@gmail.com>
2017-11-21 03:24:05 +0300

    extract add IT

--- project-organization/application/src/test/java/ru/faulab/javaee/design/patterns/sample/project/SampleProjectIT.java
+++ project-organization/application/src/test/java/ru/faulab/javaee/design/patterns/sample/project/SampleProjectIT.java
@@ -1,9 +1,23 @@
 package ru.faulab.javaee.design.patterns.sample.project;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(Arquillian.class)
 public class SampleProjectIT
 {
+    @Deployment
+    public static JavaArchive createDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
     @Test
     public void dummy()
     {
