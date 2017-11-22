ca642126afcf5f92ed700181b2ca4cc3c1cd96dd
Anton Tregubov <tregubov@gmail.com>
2017-11-22 03:08:52 +0300

    run test on build artifact

--- project-organization/application/src/test/java/ru/faulab/javaee/design/patterns/sample/project/SampleProjectIT.java
+++ project-organization/application/src/test/java/ru/faulab/javaee/design/patterns/sample/project/SampleProjectIT.java
@@ -1,26 +1,36 @@
 package ru.faulab.javaee.design.patterns.sample.project;
 
+import com.google.common.base.Preconditions;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.net.URL;
+
 @RunWith(Arquillian.class)
-public class SampleProjectIT
-{
+public class SampleProjectIT {
     @Deployment
-    public static JavaArchive createDeployment()
-    {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    public static EnterpriseArchive createDeployment() {
+        String testingArtifact = System.getProperty("testingArtifact");
+        Preconditions.checkNotNull(testingArtifact, "\"testingArtifact\" java property missing");
+        EnterpriseArchive enterpriseArchive = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, new File(testingArtifact));
+        enterpriseArchive.getAsType(WebArchive.class, path -> path.get().endsWith(".war"))
+                .stream().findFirst().ifPresent(webArchive -> webArchive.addClass(SampleProjectIT.class));
+        return enterpriseArchive;
     }
 
+    @ArquillianResource
+    URL applicationUrl;
+
+
     @Test
-    public void dummy()
-    {
-        System.out.println("sdf");
+    public void dummy() {
+        System.out.println("sdf" + applicationUrl);
     }
 }
