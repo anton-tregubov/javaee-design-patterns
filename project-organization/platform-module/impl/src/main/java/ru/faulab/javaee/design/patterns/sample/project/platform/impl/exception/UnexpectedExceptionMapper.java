9fcb39a46e0f36cb5494a6e4d08e2e5a73cb9817
Anton Tregubov <tregubov@gmail.com>
2017-12-21 02:17:06 +0300

    Ui Tests. Fix TomEE

--- project-organization/platform-module/impl/src/main/java/ru/faulab/javaee/design/patterns/sample/project/platform/impl/exception/UnexpectedExceptionMapper.java
+++ project-organization/platform-module/impl/src/main/java/ru/faulab/javaee/design/patterns/sample/project/platform/impl/exception/UnexpectedExceptionMapper.java
@@ -1,26 +1,22 @@
 package ru.faulab.javaee.design.patterns.sample.project.platform.impl.exception;
 
+import com.google.common.base.Throwables;
 import ru.faulab.javaee.design.patterns.sample.project.platform.expection.ErrorValueObject;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 @Provider
 public class UnexpectedExceptionMapper extends AbstractExceptionMapper implements ExceptionMapper<Throwable> {
     @Override
     public Response toResponse(Throwable exception) {
 
-        StringWriter errorStackTrace = new StringWriter();
-        exception.printStackTrace(new PrintWriter(errorStackTrace));
-
         return handleAcceptType(Response.serverError()).entity(
                 ErrorValueObject.builder()
                         .errorCode(1)
                         .userMessage("Oops. Contact with your administrator")
-                        .developerMessage(errorStackTrace.toString()) /*logging OoS*/
+                        .developerMessage(Throwables.getStackTraceAsString(exception)) /*logging OoS*/
                         .build())
                 .build();
     }
